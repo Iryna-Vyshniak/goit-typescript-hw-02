2dc042ecb86d36a5b588d5aaa5413f7714498c4d
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-10-03 18:15:40 +0300

    refactor: added custom types

--- src/baseTypes/4.ts
+++ src/baseTypes/4.ts
@@ -3,6 +3,8 @@
   щоб гарантувати, що перший елемент завжди буде рядком, а другий числом?
 */
 
-let person: [string, number] = ['Max', 21];
+type Person = [string, number]
+
+let person: Person = ['Max', 21];
 
 export { };
